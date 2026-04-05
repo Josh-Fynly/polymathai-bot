b9662d13f24f4cd4baf2812e786a86d092ae18fc
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-04-05 18:08:59 +0100

    Add caching functions for audio data

--- modules/cacheManager.js
+++ modules/cacheManager.js
@@ -1,1 +1,15 @@
+import { db } from "../config/firebase.js";
 
+const CACHE = "audioCache";
+
+export async function checkCache(hash) {
+  const doc = await db.collection(CACHE).doc(hash).get();
+  return doc.exists ? doc.data().result : null;
+}
+
+export async function saveCache(hash, result) {
+  await db.collection(CACHE).doc(hash).set({
+    result,
+    timestamp: Date.now(),
+  });
+}
