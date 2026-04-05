9fa4ecd0ef5d3e8f5080e6752f06448bc31f94b6
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-04-05 18:09:56 +0100

    Add user management functions for Firebase

--- modules/userManager.js
+++ modules/userManager.js
@@ -1,1 +1,24 @@
+import { db } from "../config/firebase.js";
 
+const USERS = "users";
+
+export async function getUser(phone) {
+  const doc = await db.collection(USERS).doc(phone).get();
+  return doc.exists ? doc.data() : null;
+}
+
+export async function updateUsage(phone) {
+  const ref = db.collection(USERS).doc(phone);
+
+  await db.runTransaction(async (t) => {
+    const doc = await t.get(ref);
+
+    if (!doc.exists) {
+      t.set(ref, { dailyUsage: 1, isPaid: false });
+    } else {
+      t.update(ref, {
+        dailyUsage: (doc.data().dailyUsage || 0) + 1,
+      });
+    }
+  });
+}
